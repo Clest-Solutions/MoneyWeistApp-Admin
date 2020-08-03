646b4a70cc21817dd6ff0212ef484e348f75ef69
Marcellinus2000 <acquayemarcellinus@gmail.com>
2020-08-03 12:12:02 +0000

    first commit

---
+++ mw-dashboard/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Menu from "./components/Menu";
+import Content from "./components/Content";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Menu />
+      <Content />
+    </div>
+  );
+}
